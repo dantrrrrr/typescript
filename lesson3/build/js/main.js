83b7cf1fe46ffaefff5128fc431c3a95a7b754af
dantr <huynhngdantr@gmail.com>
2023-03-24 10:44:35 +0700

    ls3 type

--- lesson3/build/js/main.js
+++ lesson3/build/js/main.js
@@ -20,6 +20,35 @@ console.log(typeof myObj);
 myObj = bands;
 myObj = {};
 const exampleObj = {
-    prop1: 'dantr',
-    prop2: true
+    prop1: "dantr",
+    prop2: true,
 };
+exampleObj.prop2 = false;
+let evh = {
+    name: "dantr",
+    active: true,
+    albums: ["new", "old", 6969],
+};
+let jb = {
+    name: "hndt",
+    active: false,
+    albums: ["cu", "moi", 879, 978],
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    }
+    return "hello undefined";
+};
+console.log(greetGuitarist(evh));
+// console.log(greetGuitarist(jb));
+//enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 3] = "U";
+    Grade[Grade["D"] = 4] = "D";
+    Grade[Grade["C"] = 5] = "C";
+    Grade[Grade["B"] = 6] = "B";
+    Grade[Grade["A"] = 7] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- lesson3/src/main.ts
+++ lesson3/src/main.ts
@@ -15,14 +15,50 @@ let myTuples: [string, number, boolean] = ["dantr", 2839, true];
 
 let mixed = ["john", 1, false];
 mixed = myTuples;
-myTuples[1]=982
+myTuples[1] = 982;
 // object
-let myObj :object ;
-myObj=[]
-console.log(typeof myObj)
-myObj=bands;
-myObj={}
-const exampleObj ={
-    prop1:'dantr',
-    prop2:true
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+  prop1: "dantr",
+  prop2: true,
+};
+exampleObj.prop2 = false;
+//////----------------------------------------------------------------
+
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
+let evh: Guitarist = {
+  name: "dantr",
+  active: true,
+  albums: ["new", "old", 6969],
+};
+let jb: Guitarist = {
+  name: "hndt",
+  active: false,
+  albums: ["cu", "moi", 879, 978],
+};
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}`;
+  }
+  return "hello undefined";
+};
+console.log(greetGuitarist(evh));
+// console.log(greetGuitarist(jb));
+
+//enums
+enum Grade {
+  U=3,
+  D,
+  C,
+  B,
+  A,
 }
+console.log(Grade.U)
